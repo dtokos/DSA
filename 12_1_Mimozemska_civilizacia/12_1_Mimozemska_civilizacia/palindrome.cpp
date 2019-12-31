fef379f8a70a7fdec16e8e8637779776e653e5f4
Dobroslav Tokoš <dtokos@zoznam.sk>
2019-12-31 20:55:27 +0100

    Fixed last test in 12_1

--- 12_1_Mimozemska_civilizacia/12_1_Mimozemska_civilizacia/palindrome.cpp
+++ 12_1_Mimozemska_civilizacia/12_1_Mimozemska_civilizacia/palindrome.cpp
@@ -1,35 +1,23 @@
 #include "palindrome.hpp"
 
+inline void findLongestLocalPalindrome(char *string, int length, int high, int low, int *maxLength, int *maxStart);
+
 int length(char *string, int length) {
-	unsigned char table[length][length];
-	memset(table, 0, sizeof(table) * sizeof(unsigned char));
 	int maxLength = 1, maxStart = 0;
 	
-	for (int i = 0; i < length; i++)
-		table[i][i] = 1;
-	
-	for (int i = 0; i < length - 1; i++) {
-		if (string[i] == string[i + 1]) {
-			table[i][i + 1] = 1;
-			maxStart = i;
-			maxLength = 2;
-		}
-	}
-	
-	for (int subLength = 3; subLength <= length; subLength++) {
-		for (int subStart = 0; subStart < length - subLength + 1; subStart++) {
-			int subEnd = subStart + subLength - 1;
-			
-			if (table[subStart + 1][subEnd - 1] && string[subStart] == string[subEnd]) {
-				table[subStart][subEnd] = 1;
-				
-				if (subLength > maxLength) {
-					maxStart = subStart;
-					maxLength = subLength;
-				}
-			}
-		}
+	for (int i = 1; i < length; i++) {
+		findLongestLocalPalindrome(string, length, i, i - 1, &maxLength, &maxStart);
+		findLongestLocalPalindrome(string, length, i + 1, i - 1, &maxLength, &maxStart);
 	}
 	
 	return maxLength;
+}
+
+void findLongestLocalPalindrome(char *string, int length, int high, int low, int *maxLength, int *maxStart) {
+	for (; low >= 0 && high < length && string[low] == string[high]; low--, high++) {
+		if (high - low + 1 > *maxLength) {
+			*maxStart = low;
+			*maxLength = high - low + 1;
+		}
+	}
 }
